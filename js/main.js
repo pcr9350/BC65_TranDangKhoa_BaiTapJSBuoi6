1c976faf2c4972c770d2aba7c84555e0a2e348a0
pcr9350 <pcr9350@gmail.com>
2024-02-29 01:15:56 +0700

    Update Bài 2

--- js/main.js
+++ js/main.js
@@ -13,8 +13,29 @@ function handleNumber() {
         number++;
         tong = tong + number;
     };
-    document.getElementById("result1").innerHTML = `Số n nhỏ nhứt để 1 + 2 + ... + n < 10000 là ${number}`;
+    document.getElementById("result1").innerHTML = `<h5>Số n nhỏ nhứt để 1 + 2 + ... + n < 10000 là ${number}</h5>`;
 };
 document.getElementById("handleNumber").onclick = function() {
     handleNumber();
+}
+
+/*-----Bài 2------
+Nhập 2 số x,n. Tính tổng S = x + x^2 + x^3 ... + x^n
+b1: 
+ Nhập 2 số x,n
+ var sum
+b2: xử lý bằng for
+b3:xuất ra file html
+*/
+function handleSum() {
+    var numX = +document.getElementById("numX").value;
+    var numN = +document.getElementById("numN").value;
+    var sum = 0;
+    for(var i = 1; i <= numN; i++) {
+        sum += Math.pow(numX, i);
+    };
+    document.getElementById("result2").innerHTML = `<h5>Tổng là: ${sum}</h5>`;
+};
+document.getElementById("handleSum").onclick = function() {
+    handleSum();
 }
